fcef1694bccc594fe650c84b13ea2deaaa850598
Magali Milbergue <magali.milbergue@gmail.com>
2019-09-20 15:11:57 +0200

    commit ejbfehb

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -10,25 +10,39 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     /**
-	 * @Route("/login", name="login")
-	 *
-	 */
-	public function login(AuthenticationUtils $auth)
-	{
+     * @Route("/login", name="login")
+     *
+     */
+    public function login(AuthenticationUtils $auth)
+    {
 
-		$lastUsername = $auth->getLastUsername();
-		// récupérer le pseudo qui a été utilisé la dernière fois
+        $lastUsername = $auth->getLastUsername();
+        // récupérer le pseudo qui a été utilisé la dernière fois
 
-		$error = $auth->getLastAuthenticationError();
-		// récupérer les erreurs
+        $error = $auth->getLastAuthenticationError();
+        // récupérer les erreurs
 
-		if (!empty($error)) {
-			$this->addFlash('errors', 'Problème d\'identifiant !');
-		}
+        if (!empty($error)) {
+            $this->addFlash('errors', 'Problème d\'identifiant !');
+        }
 
 
-		return $this->render('security/login.html.twig', [
-			'lastUsername' => $lastUsername
-		]);
-	}
+        return $this->render('security/login.html.twig', [
+            'lastUsername' => $lastUsername
+        ]);
+    }
+
+    /**
+     * @Route("/deconnexion", name="deconnexion")
+     *
+     */
+    public function deconnexion()
+    { }
+
+    /**
+     * @Route("/connexion_check", name="connexion_check")
+     *
+     */
+    public function connexionCheck()
+    { }
 }
