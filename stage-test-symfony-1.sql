c97f2ae5cee0482cb682345059832c0f4cccf79d
Marine <marinepelletier@me.com>
2019-09-23 16:25:44 +0200

    GOOD GOOD GOOD
    ou pas.

--- stage-test-symfony-1.sql
+++ stage-test-symfony-1.sql
@@ -1,71 +1,27 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le :  lun. 23 sep. 2019 à 13:25
--- Version du serveur :  5.7.26
--- Version de PHP :  7.3.5
+-- Host: localhost:8889
+-- Generation Time: Sep 23, 2019 at 02:24 PM
+-- Server version: 5.7.26
+-- PHP Version: 7.3.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
--- Base de données :  `stage-test-symfony-1`
+-- Database: `stage-test-symfony-1`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `contact`
+-- Table structure for table `entreprise`
 --
 
-DROP TABLE IF EXISTS `contact`;
-CREATE TABLE IF NOT EXISTS `contact` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nom` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `prenom` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `societe` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `telephone` varchar(17) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `objet` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `message` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `contenu`
---
-
-DROP TABLE IF EXISTS `contenu`;
-CREATE TABLE IF NOT EXISTS `contenu` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `section` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `titre` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `sous_titre` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `texte` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `entreprise`
---
-
-DROP TABLE IF EXISTS `entreprise`;
-CREATE TABLE IF NOT EXISTS `entreprise` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `entreprise` (
+  `id` int(11) NOT NULL,
   `nom` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `statut_rcs` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `adresse` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -77,140 +33,32 @@ CREATE TABLE IF NOT EXISTS `entreprise` (
   `siren` int(11) DEFAULT NULL,
   `siret` int(11) DEFAULT NULL,
   `activite` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `nom_gerant` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `galerie`
---
-
-DROP TABLE IF EXISTS `galerie`;
-CREATE TABLE IF NOT EXISTS `galerie` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `photo` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Déchargement des données de la table `galerie`
---
-
-INSERT INTO `galerie` (`id`, `photo`) VALUES
-(1, 'test-photo-1.jpg'),
-(2, 'test-photo-1.jpg'),
-(3, 'test-photo-1.jpg'),
-(4, 'test-photo-2.jpg');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `migration_versions`
---
-
-DROP TABLE IF EXISTS `migration_versions`;
-CREATE TABLE IF NOT EXISTS `migration_versions` (
-  `version` varchar(14) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `executed_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
-  PRIMARY KEY (`version`)
+  `nom_gerant` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Déchargement des données de la table `migration_versions`
+-- Dumping data for table `entreprise`
 --
 
-INSERT INTO `migration_versions` (`version`, `executed_at`) VALUES
-('20190923062313', '2019-09-23 06:23:40'),
-('20190923070546', '2019-09-23 07:05:55'),
-('20190923080344', '2019-09-23 08:04:01');
-
--- --------------------------------------------------------
+INSERT INTO `entreprise` (`id`, `nom`, `statut_rcs`, `adresse`, `cp`, `ville`, `telephone`, `mail_gerant`, `mail_contact`, `siren`, `siret`, `activite`, `nom_gerant`) VALUES
+(1, 'Entreprise MBMP', NULL, '1 Chemin de la Salle', 75001, 'Paris', '0123456789', 'test.mbmp@gmail.com', 'test.mbmp@gmail.com', NULL, NULL, 'Société qui tente de coder', NULL);
 
 --
--- Structure de la table `partenaires`
+-- Indexes for dumped tables
 --
 
-DROP TABLE IF EXISTS `partenaires`;
-CREATE TABLE IF NOT EXISTS `partenaires` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nom_partenaire` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `site_partenaire` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `logo` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+--
+-- Indexes for table `entreprise`
+--
+ALTER TABLE `entreprise`
+  ADD PRIMARY KEY (`id`);
 
 --
--- Déchargement des données de la table `partenaires`
+-- AUTO_INCREMENT for dumped tables
 --
 
-INSERT INTO `partenaires` (`id`, `nom_partenaire`, `site_partenaire`, `logo`) VALUES
-(1, 'TEST1', 'http://wwww.google.com', 'logo1.jpg'),
-(2, 'TEST2', 'http://wwwW.yahoo.com', 'logo2.jpg'),
-(3, 'test3', 'http://wwww.amazon.fr', 'logo3.jpg'),
-(4, 'test4', 'http://www.fnac.com', 'logo4.jpg'),
-(5, 'test5', 'http://www.imdb.com', 'logo5.jpg');
-
--- --------------------------------------------------------
-
 --
--- Structure de la table `specificites`
+-- AUTO_INCREMENT for table `entreprise`
 --
-
-DROP TABLE IF EXISTS `specificites`;
-CREATE TABLE IF NOT EXISTS `specificites` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `facebook` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `instagram` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `page_google` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `localisation1` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `localisation2` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `localisation3` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `localisation4` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `localisation5` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `localisation6` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `localisation7` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `localisation8` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `localisation9` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `localisation10` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `competence1` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `competence2` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `competence3` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `competence4` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `competence5` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `competence6` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `competence7` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `competence8` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `competence9` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `competence10` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `user`
---
-
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `role` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `UNIQ_8D93D649E7927C74` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Déchargement des données de la table `user`
---
-
-INSERT INTO `user` (`id`, `email`, `password`, `role`) VALUES
-(1, 'magali@gmail.com', '$argon2i$v=19$m=65536,t=4,p=1$RWhLbll4R0tmOEZVaGQzVw$Bo4HOBpT454C0Pim2Fp6DchuW1jkhVRECIlfBHXDnwI', 'ROLE_ADMIN');
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+ALTER TABLE `entreprise`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
