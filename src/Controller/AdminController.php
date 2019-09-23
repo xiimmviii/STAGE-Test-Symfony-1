732ee839c50f53ee8a3fe3dc6150b254cca83a2f
Marine <marinepelletier@me.com>
2019-09-23 13:30:09 +0200

    FULL SKELETON

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -16,4 +16,86 @@ class AdminController extends AbstractController
             'controller_name' => 'AdminController',
         ]);
     }
+
+    /**
+     * @Route("/admin/entreprise", name="entreprise")
+     */
+    public function showEntreprise()
+    {
+        return $this->render('admin/entreprise.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+        /**
+     * @Route("/admin/galeriephotos", name="galeriephotos")
+     */
+    public function galeriePhoto()
+    {
+        return $this->render('admin/galeriephotos.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+        /**
+     * @Route("/admin/presensation-entreprise", name="presentationentreprise")
+     */
+    public function presentationEntreprise()
+    {
+        return $this->render('admin/presentation-entreprise.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+        /**
+     * @Route("/admin/barre-de-labels", name="barredelabels")
+     */
+    public function barreLabels()
+    {
+        return $this->render('admin/barre-de-labels.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+        /**
+     * @Route("/admin/histoire-entreprise", name="histoireentreprise")
+     */
+    public function histoireEntreprise()
+    {
+        return $this->render('admin/histoire-entreprise.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+        /**
+     * @Route("/admin/competences", name="competences")
+     */
+    public function competences()
+    {
+        return $this->render('admin/competences.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+        /**
+     * @Route("/admin/partenaires", name="partenaires")
+     */
+    public function partenaires()
+    {
+        return $this->render('admin/partenaires.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+        /**
+     * @Route("/admin/contact-localisation", name="contactlocalisation")
+     */
+    public function contactLocalisation()
+    {
+        return $this->render('admin/contact-localisation.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+
 }
