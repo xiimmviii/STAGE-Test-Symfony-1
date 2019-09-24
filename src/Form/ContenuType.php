ca51a1e8192a01ab67daa2b4e519361de5653dd8
Marine <marinepelletier@me.com>
2019-09-24 15:47:46 +0200

    Create ContenuType.php

---
+++ src/Form/ContenuType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Specificites;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class SpecificitesType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+        ->add('section', TextType::class)
+            ->add('titre', TextType::class)
+            ->add('sousTitre', TextType::class,array('required' => false))
+            ->add('texte', TextareaType::class)
+            ->add('sousTitre', TextType::class,array('required' => false, 'empty_data' => '0',))
+        
+            ->add('submit', SubmitType::class);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+
+        ]);
+    }
+}
