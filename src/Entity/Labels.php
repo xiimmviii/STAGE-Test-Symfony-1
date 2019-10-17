c996b4209353f5b61d4c8b9631c49a1303634c1d
Magali Milbergue <magali.milbergue@gmail.com>
2019-10-17 10:51:47 +0200

    FAIRE MIGRATION

--- src/Entity/Labels.php
+++ src/Entity/Labels.php
@@ -22,7 +22,7 @@ class Labels
     private $nom;
 
     /**
-     * @ORM\ManyToOne(targetEntity="App\Entity\icons", inversedBy="labels")
+     * @ORM\ManyToOne(targetEntity="App\Entity\Icons", inversedBy="labels")
      * @ORM\JoinColumn(nullable=false)
      */
     private $svg_nom;
