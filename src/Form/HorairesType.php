e8c52f220d5e653447b47145c946e17e3b8f43cf
Marine <marinepelletier@me.com>
2019-10-17 16:27:47 +0200

    MAJ Horaires !

--- src/Form/HorairesType.php
+++ src/Form/HorairesType.php
@@ -24,12 +24,12 @@ class HorairesType extends AbstractType
             // Required permet de préciser si le contenu est obligatoire (true) ou non (false)
             
             ->add('Ouverture', TimeType::class, [
-                'input'  => 'datetime_immutable',
+                'input'  => 'datetime',
                 'widget' => 'choice',
                 'required' => true
             ])
             ->add('Fermeture', TimeType::class, [
-                'input'  => 'datetime_immutable',
+                'input'  => 'datetime',
                 'widget' => 'choice',
                 'required' => true
             ])
